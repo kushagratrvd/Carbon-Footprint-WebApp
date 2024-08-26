9a0b2c51859593cf23e6ebd3946b87b70b4e6eab
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2024-08-26 22:32:48 +0530

    demo data

---
+++ ecommerce/assets/demoData.js
@@ -0,0 +1,192 @@
+const demoData = [
+
+    {
+        title: "Beco Eco-Friendly Baking <br> & Wrapping Paper ",
+        price: 159,
+        category: "Wear",
+        img: "./assets/img/d4.webp",
+        tags: ["New", "Apple", '2024'],
+        desc: "Beco Bake & Wrap paper is a versatile and eco-friendly alternative to traditional baking paper. Made from 100% wood pulp, it is unbleached, chlorine-free, and silicone-free, making it a safe and sustainable choice for both baking and wrapping. This multifunctional paper can be used for various purposes, from lining baking sheets and creating pastry bags to steaming foods and wrapping them for storage or takeaway. It is compatible with a wide range of appliances, including microwaves, ovens, grills, OTGs, air-fryers, and steamers, and is safe to use at temperatures up to 240°C. Additionally, Beco Bake & Wrap paper is biodegradable, breaking down naturally within 180 days, making it an environmentally responsible choice for your kitchen..",
+
+        onsale: false,
+    },
+    {
+        title: "The Bamboo Bae Hand Stiched<br> Coconut Coir Scrubbers ",
+        price: 299,
+        category: "Daily Used Products",
+        img: "./assets/img/d3.webp",
+        tags: ["New", '2024'],
+        desc: "Our coconut fiber scrubbers are designed for effective cleaning, fitting comfortably in your hand to provide a good grip and coverage, so you can spend less time scrubbing dishes. Unlike conventional nylon scrub pads, these eco-friendly scrubbers are made from all-natural materials, making them a sustainable choice for your kitchen. They are gentle yet powerful, allowing you to clean anything from glass and stainless steel to cast iron, non-stick coatings, copper, and chrome without leaving scratches. Thanks to the unique quick-dry properties of coconut fibers, these scrubbers remain odor and smell-free, preventing mold when stored in a dry place after use..",
+
+        onsale: false,
+    },
+    {
+        title: "Baal Pure Neem Wood Comb<br>For Women and Men",
+        price: 199,
+        img: "./assets/img/d2.webp",
+        category: "Daily Used Products",
+        tags: ["New", '2024'],
+        desc: "Our handcrafted neem wood combs offer a natural solution to hair care, leveraging the healing properties of the neem plant. Known for its ability to reduce dandruff and hair fall, neem provides a soothing effect with every stroke. We offer a variety of comb styles, including wide-tooth combs, tail combs, handle combs, and double-sided combs, all designed to improve blood circulation and promote a healthy scalp. With smooth edges to prevent tugging or pulling, these combs are suitable for all hair types, whether long, short, curly, straight, thin, or thick. Ideal for both men and women, these combs are highly recommended by professional hairstylists as an eco-friendly and non-toxic alternative. Each comb is meticulously handcrafted with a luxury-grade finish using fine-quality bamboo, ensuring no unpleasant odors. Go earth-friendly and hair-friendly with our premium neem wood combs.",
+        onsale: false,
+    },
+    {
+        title: "Beco Bamboo Toothbrush <br> Pack of 2",
+        price: 79,
+        img: "./assets/img/d1.jpg",
+        category: "Daily Used Products",
+        tags: ["New", '2024'],
+        desc: "Our bamboo toothbrushes are a natural, eco-friendly choice for oral hygiene. Made from 100% organic bamboo, they provide a sustainable alternative to traditional plastic toothbrushes. The soft bristles are carefully infused with bamboo charcoal, which helps absorb odor-causing agents, leaving your mouth fresh and aiding in deep teeth cleaning and whitening. The toothbrushes are BPA-free and gentle on teeth enamel and gums. With a well-designed handle, they offer a comfortable grip and ease of use, ensuring a long-lasting, splinter-free experience. Dentists recommend replacing your toothbrush every 3-4 months or when the bristles lose their stiffness. Completely biodegradable, these toothbrushes are an environmentally friendly choice for a cleaner planet.",
+
+        onsale: false,
+    },
+    {
+        title: "Red Butler Eco-Friendly <br> Bamboo Fibre Dinner Sets <br>Set of 12 Piece- <br>Dinner Plates, Bowls & Tumblers ",
+        price: 2697,
+        img: "./assets/img/k4.jpg",
+        category: "Kitchen Product",
+        desc: "Our bamboo plates offer a chemical-free dining experience, ensuring health and safety with every meal. Lightweight, unbreakable, and stain-resistant, these plates are perfect for everyday use, providing practicality for daily meals. They are designed with food-grade natural colors, adding a stylish touch to your dining table. Made from eco-friendly and biodegradable bamboo, these plates support sustainability efforts while being easy to use and maintain, making them ideal for busy lifestyles. For added convenience, they are microwave and dishwasher safe, offering versatility and ease of cleaning. With each wash, these plates gain a beautiful shine, ensuring they remain fresh and attractive over time..",
+
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: " Reusable Bamboo Toast <br>Tongs Wooden Toaster<br> Tongs for Cooking",
+        price: 72,
+        img: "./assets/img/k3.webp",
+        category: "Kitchen Product",
+        tags: ["New", '2024'],
+        desc: "Our high-quality bamboo kitchen tongs are crafted from natural bamboo with a solid, durable build that is designed to last. These 8-inch tongs are perfect for various kitchen tasks, such as grabbing toast from the toaster, serving sweets at parties, cooking bacon, flipping barbecue meat, serving salad, or even as a substitute for chopsticks. Their ergonomic design makes them easy to use, allowing you to grip toast, bagels, and more with elegance and style. Made from non-absorptive bamboo, these tongs are stain and odor-resistant, making them easy to clean with warm soapy water. Plus, they are reusable and eco-friendly, ensuring long-lasting use in your kitchen. Whether you’re hosting a party or looking for a thoughtful gift, these bamboo tongs are a versatile tool that combines functionality with style. We’re confident you’ll love them, but if for any reason you’re not satisfied, we offer a risk-free guarantee with a full refund.",
+
+        onsale: false,
+    },
+    {
+        title: "Rusabl Bamboo Wooden <br>Chopping Board",
+        price: 449,
+        img: "./assets/img/k2.webp",
+        category: "Kitchen Product",
+        tags: ["New", '2024'],
+        desc: "This eco-friendly wooden chopping board is expertly crafted from non-toxic, food-grade natural bamboo, ensuring a safe and durable surface for all your food preparation needs. Measuring 34 x 24 x 1.8 cm, its generous size is perfect for cutting, chopping, and prepping food on a single surface. The bamboo construction offers natural anti-bacterial properties without any added dyes or stains, making it a great choice for food safety. Additionally, the board is gentle on expensive knives, unlike glass or plastic alternatives that can dull their edges. The premium metal handle not only enhances your kitchen’s aesthetics but also makes it easy to carry and hang on wall hooks. To prolong the life of this chopping board, avoid submerging it in water. Instead, clean it with mild soap and water and dry it thoroughly after each use.",
+
+        onsale: false,
+    },
+    {
+        title: "Beco Reusable Bamboo Kitchen <br> Towel/Tissue Roll ",
+        price: 275,
+        img: "./assets/img/k1.jpg",
+        category: "Kitchen Product",
+        desc: "🌿REUSABLE - These 11 x 12perforated tear off sheets are machine washable and reusable up to 100 times before discarding. Use these bamboo towels just like how you normally use conventional paper towels. Hand or machine wash and air dry for best results and longer life.<br>🌿WASHABLE, & TREE FREE: Our rolls are made from 100% organic bamboo, which is a fast growing grass giving a sustainable, natural alternative to traditional tree based kitchen paper towels. Unlike trees which can take up to 30 years to grow, bamboo grows back in as quickly as 3 to 4 months.<br>🌿STRONG, DURABLE, & SUPER ABSORBENT: Our super absorbent single ply bamboo sheets are strong and durable. Each sheet lasts up to 100 uses making each bamboo roll equivalent to 20 regular paper rolls. You save both on space and usage.<br>🌿CHEMICAL FREE & ECO-FRIENDLY: Our bamboo rolls are hypoallergenic, BPA free, paraben free, and chlorine free. They are biodegradable and does not contribute to deforestation unlike tree paper rolls<br>🌿SAVE MONEY - One roll of our bamboo towels replaces 60 conventional paper towel rolls. That's up to 6 months of paper towels! Using bamboo towels decreases 3,000 tons of paper towel waste a day. Save trees while saving space and money with this eco-friendly sustainable alternative.",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: " Scalp Massager Shampoo Brush",
+        price: 149,
+        img: "./assets/img/e1.webp",
+        category: "Daily Products",
+        tags: ["New", '2024'],
+        desc: "Our eco-friendly scalp massager shampoo brush is crafted from durable wheat straw material, offering a sturdy and biodegradable option that’s also anti-bacterial, anti-odor, and non-toxic. The soft silicone bristles are designed to gently clean and massage the scalp, helping to prevent dandruff, improve blood circulation, and promote hair growth by strengthening hair roots and keeping hair healthy. This brush also provides stress and headache relief through its soothing scalp massage, promoting relaxation of the scalp muscles. With an ergonomic design and perfect grip, it is easy to hold and simple to use, requiring no batteries for operation. Waterproof, portable, and compact, this scalp massager is ideal for use at home or on the go.",
+
+        onsale: false,
+    },
+    {
+        title: "Organic B Bamboo Tribal Travel Kit",
+        price: 350,
+        img: "./assets/img/e2.webp",
+        category: "Daily Used Products",
+        desc: "The Bamboo Toothbrush is charcoal-infused for natural teeth cleaning on the go, while the Bamboo Tongue Cleaner offers compact and sustainable oral hygiene. The Pocket Neem Comb provides portable grooming with eco-friendly neem wood, and the Bamboo Straw is perfect for sustainable sipping. Together, these items form the Tribal Travel Kit—a comprehensive set designed for eco-conscious travelers who prioritize sustainability and convenience.",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: "Organic Clay Eco-Friendly Bottle (1 Litre) ",
+        price: 287,
+        img: "./assets/img/b1.webp",
+        category: "Bottles",
+        desc: "A human body needs at least 2 liters of pure water every day for its healthy functioning. It won't be easier to find a way to keep your drinking water cool and healthy we understand your needs and brought you these terracotta water bottles to fulfill your needs. These water bottles made of terracotta could be the best way to replace the harmful plastic, metal, or the fragile glassware that you are currently using. Self Cooling Bottle: These organic clay water bottles have self ability to keep your water cool and healthy. This bottle keeps liquid as it is for hours, also the bottle is lightweight so you can take it along with you to work or college. Elegant Shape: The shape of the bottle is designed keeping in mind the comfort of holding it. The clay body of the bottle has a storage capacity of 1 litre. A high-quality lid that ensures no leakage is designed perfectly for this bottle. Healthy and Natural taste: Plastic bottles are not good for health that's why we brought the solution of that. These water bottles will not only give you fresh and cool water but will also give you a healthy and natural taste of water. Improve Digestion & Metabolism: These water bottle helps to improve the digestion and increase metabolism..",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: "Obvie 1L Water bottle For Sports,<br> BPA Free Eco-friendly Plastic<br> Drinking Bottle ",
+        price: 598,
+        img: "./assets/img/b2.webp",
+        category: "Bottle",
+        desc: "Obvie Sport Water Bottle Capacity Marker & Lightweight: This 1000ml water bottles with capacity markers, translucent bottle can see your daily water consumption. Remind you to stay hydrated and drink enough water, better achieve fitness goals. It is also lightweight but durable to use. Perfect for running, hiking, cycling, travel and any outdoor activities. BPA FREE & Convenient Design: US1984 water bottle with capacity marker is made of quality food-grade plastic, 100% BPA & TOXIN FREE, odour free and healthy for your daily water drinking. The sports water bottle featured with a portable wrist strap design, which makes you easier to carry the water bottle to anywhere. Ideal Gift Choice & Use Tips: This 1 liter large capacity and a variety of bright colors motivational sports water bottles is the best gift for yourself, friends, kids, family, colleagues, women and men who enjoy a healthy lifestyle gift, help them keep hydrated and healthy! Use tips: Do not drop it; Not dishwasher safe; Clean with baking soda and vinegar.Specifications",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: " Irida Naturals UNBREAKABLE & <br>ECO-FRIENDLY lightweight<br> water bottle",
+        price: 549,
+        img: "./assets/img/b3.webp",
+        category: "Bottle",
+        desc: "Irida Naturals is a company that sells sustainable homeware and kitchenware. At Irida Naturals, we believe that it is our duty, as a community, to reduce the gaping levels of plastic consumption and disposal. We have found environmentally friendly alternatives to our regular kitchenware and homeware items without compromising on quality or durability.",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: "Eco Friendly VBA / Bamboo<br> Bottle Hot & Cold <br>For Regular  ",
+        price: 650,
+        img: "./assets/img/b4.webp",
+        category: "Bottle",
+        desc: "Regular Use Bamboo Bottle Hot & Cold Upto 2-4 Hours.",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: "Jute Grow Bags (6x6x6 inch) ",
+        price: 160,
+        img: "./assets/img/bag4.webp",
+        category: "Bags",
+        tags: ["New", '2024'],
+        desc: "Add a vibrant touch to your daily routine with our Red  Jute Bag. Made from natural, eco-friendly jute fibers, this bag combines sustainability with bold style. Its rich red color ads a pop of brightness, making it both eye-catching and practical. The sturdy jute material ensures durability, while the robust handles provide comfort and strength for carrying groceries, beach essentials, or everyday items. Versatile and functional, this jute bag is perfect for any occasion, from shopping trips to casual outings. Embrace an eco-friendly lifestyle with a splash of color, and make a statement with every use.",
+
+        onsale: false,
+    },
+    {
+        title: "6 pieces of Designer Jute Bags ",
+        price: 593,
+        img: "./assets/img/bsd.jpg",
+        category: "Bags",
+        tags: ["New", '2024'],
+        desc: "Introducing our set of 6 Designer Jute Bags, crafted to blend elegance with eco-consciousness. Each bag is made from high-quality, natural jute fibers, providing a durable and sustainable alternative to plastic. With unique, stylish designs, these bags are perfect for any occasion, whether you're heading to the market, the beach, or just running errands. The set includes a variety of colors and patterns, ensuring you have the perfect bag to match your outfit or mood. Each bag features comfortable handles for easy carrying and a spacious interior to accommodate all your essentials. Durable and versatile, these designer jute bags are not only practical but also a chic statement in promoting an eco-friendly lifestyle. Elevate your daily routines with these beautifully crafted bags and make a positive impact on the environment.",
+
+        onsale: false,
+    },
+    {
+        title: "Red-Orange Color Jute Bags",
+        price: 190,
+        img: "./assets/img/bag3.webp",
+        category: "Bags",
+        desc: "Add a vibrant touch to your daily routine with our Red Orange Jute Bag. Made from natural, eco-friendly jute fibers, this bag combines sustainability with bold style. Its rich red-orange color adds a pop of brightness, making it both eye-catching and practical. The sturdy jute material ensures durability, while the robust handles provide comfort and strength for carrying groceries, beach essentials, or everyday items. Versatile and functional, this jute bag is perfect for any occasion, from shopping trips to casual outings. Embrace an eco-friendly lifestyle with a splash of color, and make a statement with every use.",
+
+        tags: ["New", '2024'],
+        onsale: false,
+    },
+    {
+        title: "Rusabl Jute Bag (Pack of 1, Beige)",
+        price: 199,
+        img: "./assets/img/bag2.webp",
+        category: "Bags",
+        tags: ["New", '2024'],
+        desc: "Choose sustainability with the Rusabl Jute Bag, made from natural fibers to offer an eco-friendly alternative to plastic bags. Its robust handle ensures durability and strength, making it a reliable option for daily use. Crafted from sturdy jute material, the Rusabl Jute Handbag is designed to withstand regular use and last over time. Versatile in its applications, this bag is ideal for grocery shopping, travel, beach outings, work, or any everyday activity. By opting for the Rusabl Jute Handbag, you actively contribute to reducing plastic waste, promoting a sustainable and eco-friendly lifestyle.",
+        onsale: false,
+    },
+    // {
+    //     title: "",
+    //     price: ,
+    //     img:"",
+    //     category: "Wear",
+    //     tags: ["New", '2024'],
+    //     onsale: false,
+    // },
+]
+
+export default demoData;
